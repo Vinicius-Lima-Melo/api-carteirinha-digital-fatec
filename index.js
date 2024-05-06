aea0e96af52d8ef07a22b6eb5eed3235fab09bdf
vinicius-lima-melo <viniciuslimamelo0@gmail.com>
2024-05-05 23:04:06 -0300

    Updates

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const fatecApi = require('fatec-api')
-const minhaConta = new fatecApi.Account('47367558811', '47367558811')
+const express = require('express')
+const app = express()
+app.use(express.json())
 
-console.log(fatecApi, minhaConta.student.getName)
-minhaConta.getName().then(() => {
-    console.log(minhaConta.student.getName());
-    // <- 'SEU NOME COM CAPSLOCK'
-  })
-// minhaConta.getName().then(nome => {
-//   console.log("Öla")
-//   // <- 'SEU NOME COM CAPSLOCK'
+// GET method route
+app.get('/', (req, res) => {
+    res.send('GET request to the homepage')
+})
+
+
+  // POST method route
+app.post('/login', async (req, res) => {
+    console.log(req.body)
+    const minhaConta = new fatecApi.Account(req.body.login, req.body.senha)
+
+    await minhaConta.getName().then((name) => {
+        res.send("Olá, " + name + "!")
+    }, (err) => {
+        res.send("Não foi possível acessar sua conta.")
+    })
+})
+
+// console.log(fatecApi, minhaConta.student.getName)
+// minhaConta.getName().then(() => {
 // })
+
+app.listen(8000, () => {
+    console.log('Servidor iniciado na porta 8000!')
+})
